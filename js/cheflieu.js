9c90502b8aa6101686393ca44e774ee2ce776739
ayman-fer <97614053+ayman-fer@users.noreply.github.com>
2022-02-28 23:54:38 +0100

    Add files via upload

---
+++ js/cheflieu.js
@@ -0,0 +1,18 @@
+var cheflieu ={
+"type": "FeatureCollection",
+"name": "Covid",
+"features": [
+{ "type": "Feature", "properties": { "FID_": 0, "Id": 12, "Région": "Eddakhla Oued Eddahab", "cheflieu": "Oued-Ed-Dahab" }, "geometry": { "type": "Point", "coordinates": [ -15.745899, 23.6426382 ] } },
+{ "type": "Feature", "properties": { "FID_": 1, "Id": 11, "Région": "Laayoun Sakia El Hamra​​", "cheflieu": "Laâyoune" }, "geometry": { "type": "Point", "coordinates": [ -13.198007363, 27.14166373 ] } },
+{ "type": "Feature", "properties": { "FID_": 2, "Id": 10, "Région": "Guelmim Oued Noun​​", "cheflieu": "Guelmim" }, "geometry": { "type": "Point", "coordinates": [ -10.0527172874, 28.991018651 ] } },
+{ "type": "Feature", "properties": { "FID_": 3, "Id": 9, "Région": "Souss Massa", "cheflieu": "Agadir-Ida-Outanane" }, "geometry": { "type": "Point", "coordinates": [ -9.5625122, 30.4253702 ] } },
+{ "type": "Feature", "properties": { "FID_": 4, "Id": 8, "Région": "Daraa Tafilalet", "cheflieu": "Errachidia" }, "geometry": { "type": "Point", "coordinates": [ -4.436896652, 31.9112059 ] } },
+{ "type": "Feature", "properties": { "FID_": 5, "Id": 2, "Région": "Oriental​​", "cheflieu": "Oujda-Angad" }, "geometry": { "type": "Point", "coordinates": [ -1.9655703455, 34.689555643 ] } },
+{ "type": "Feature", "properties": { "FID_": 6, "Id": 1, "Région": "Tanger Tétouan Al Hoceima", "cheflieu": "Tanger-Assilah" }, "geometry": { "type": "Point", "coordinates": [ -5.879087576, 35.754586645 ] } },
+{ "type": "Feature", "properties": { "FID_": 7, "Id": 3, "Région": "Fés Meknès​​", "cheflieu": "Fès" }, "geometry": { "type": "Point", "coordinates": [ -5.0149417143, 34.0375172295 ] } },
+{ "type": "Feature", "properties": { "FID_": 8, "Id": 4, "Région": "Rabat Salé Kénitra", "cheflieu": "Rabat" }, "geometry": { "type": "Point", "coordinates": [ -6.847029967, 33.98152153 ] } },
+{ "type": "Feature", "properties": { "FID_": 9, "Id": 5, "Région": "Béni Mellal Khénifra", "cheflieu": "Béni Mellal" }, "geometry": { "type": "Point", "coordinates": [ -6.375261838, 32.336789263 ] } },
+{ "type": "Feature", "properties": { "FID_": 10, "Id": 6, "Région": "Grand Casa Settat", "cheflieu": "Casablanca" }, "geometry": { "type": "Point", "coordinates": [ -7.586903979, 33.57378916 ] } },
+{ "type": "Feature", "properties": { "FID_": 11, "Id": 7, "Région": "Marrakech Safi​​", "cheflieu": "Marrakech" }, "geometry": { "type": "Point", "coordinates": [ -7.98592198, 31.64031647 ] } }
+]
+}
